8ed82b05c9b0606ed8a7a4a26e3d27b5e5d43789
Elina <khamitovaElina09@gmail.com>
2023-02-25 17:20:33 +0300

    Добавила окно с информацией об авторе изменения

---
+++ Template_4332/4332_Khamitova.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Template_4332
+{
+    /// <summary>
+    /// Interaction logic for _4332_Khamitova.xaml
+    /// </summary>
+    public partial class _4332_Khamitova : Window
+    {
+        public _4332_Khamitova()
+        {
+            InitializeComponent();
+        }
+    }
+}
