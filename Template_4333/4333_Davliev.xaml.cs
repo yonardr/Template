c20a5697882cb7e7d11250d7ac28f01b2be260b3
Almaz <52231348+Heahdix@users.noreply.github.com>
2023-02-25 12:08:27 +0300

    Добавлены элементы
    Добавил кнопку, её обработчик, окно

---
+++ Template_4333/4333_Davliev.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Template_4333
+{
+    /// <summary>
+    /// Логика взаимодействия для _4333_Davliev.xaml
+    /// </summary>
+    public partial class _4333_Davliev : Window
+    {
+        public _4333_Davliev()
+        {
+            InitializeComponent();
+        }
+    }
+}
