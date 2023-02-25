8fd8485f25fbeec28a8a6b038b5f704545b11798
Dinar <ValiakhmetovDE@stud.kai.ru>
2023-02-25 13:23:31 +0300

    msgs

---
+++ Template_4333/4333_Valiakhmetov.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Template_4333
+{
+    /// <summary>
+    /// Interaction logic for _4333_Valiakhmetov.xaml
+    /// </summary>
+    public partial class _4333_Valiakhmetov : Window
+    {
+        public _4333_Valiakhmetov()
+        {
+            InitializeComponent();
+        }
+    }
+}
