29fa15c3ff6c3d4ab86a2fe0ddc56726580b5988
AdelYar <salyakhovade@gmail.com>
2023-02-28 17:28:10 +0300

    upd frmwk

---
+++ Template_4335/Windows/Klevtsov_4335.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Template_4335.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Klevtsov_4335.xaml
+    /// </summary>
+    public partial class Klevtsov_4335 : Window
+    {
+        public Klevtsov_4335()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
