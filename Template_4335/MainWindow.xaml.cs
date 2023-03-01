2f34df447194a485945a3d9be7e5337ccb490a63
Михаил Максимов <mi2004ha@mail.ru>
2023-03-01 13:43:19 +0300

    добавление обработчиков событий по переходу по кнопке.

--- Template_4335/MainWindow.xaml.cs
+++ Template_4335/MainWindow.xaml.cs
@@ -76,7 +76,14 @@ namespace Template_4335
 
         private void Maksimov_4335_Click(object sender, RoutedEventArgs e)
         {
+            Maksimov_4335 mak = new Maksimov_4335();
+            mak.Show();
+        }
 
+        private void Akhmetova_4335_Click(object sender, RoutedEventArgs e)
+        {
+            Akhmetova_4335 ak = new Akhmetova_4335();   
+            ak.Show();
         }
     }
 }
